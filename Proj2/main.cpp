b4cd5b0a647c804c9cb41f8933fce07a974a8635
Lloyd Allen <lloydallen597@gmail.com>
2024-11-01 23:50:15 -0500

    first commit

---
+++ Proj2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "hello world Programming" << endl;
+    cout << "Enter your favorite number: " << endl;
+    cin >> num;
+    cout << "Your favorite number is " << num << endl;
+    return 0;
+}
---
+++ proj1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter a positive integer: ";
+    return 0;
+}
